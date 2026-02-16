d8a785b0af736b9abc0eabf0c00e89dc7b535fb5
Manraj Singh <manrajsingh84@gmail.com>
2026-02-16 14:01:31 -0800

    take down infra (#7)

--- variables.tf
+++ variables.tf
@@ -13,18 +13,3 @@ variable "location" {
   type        = string
   default     = "westus"
 }
-
-variable "admin_username" {
-  description = "Admin username for the VM (from GitHub repository variable)"
-  type        = string
-}
-
-variable "admin_authorized_keys" {
-  description = "List of GitHub usernames whose SSH keys should be authorized"
-  type        = list(string)
-}
-
-variable "allowed_ssh_cidrs" {
-  description = "CIDR blocks allowed to access the VM via SSH"
-  type        = list(string)
-}
