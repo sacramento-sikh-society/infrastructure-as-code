e9252c07d50556156fdb8bc597b5250b0bca16ec
Manraj Singh <ManrajSingh84@gmail.com>
2026-02-16 17:14:01 -0800

    fixing new line issue with cicd

--- providers.tf
+++ providers.tf
@@ -1,8 +1,6 @@
 provider "azurerm" {
   subscription_id = var.subscription_id
   tenant_id       = var.tenant_id
-  use_oidc        = true
-  use_cli         = false
 
   features {
     key_vault {
