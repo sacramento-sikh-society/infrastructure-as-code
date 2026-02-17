03b758c7b591bdb41c2082f7a89e7dacb7e8cd7c
Manraj Singh <ManrajSingh84@gmail.com>
2026-02-16 17:47:18 -0800

    fixing cicd

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
